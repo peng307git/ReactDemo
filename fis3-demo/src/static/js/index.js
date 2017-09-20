562f9a2c27726f72bcebf4d27f2c2191d77b7374
peng307git <2324964369@qq.com>
2017-09-20 09:33:57 +0800

    fix

---
+++ fis3-demo/src/static/js/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by qiangxl on 2017/4/12.
+ */
+import {firstName, lastName, year} from 'profile.js';
+alert(firstName)
---
+++ fis3-demo/src/static/js/profile.js
@@ -0,0 +1,5 @@
+// profile.js
+var firstName = 'Michael';
+var lastName = 'Jackson';
+var year = 1958;
+export {firstName, lastName, year};
